b58619456191d638505447cb88f00d24a3ba1c41
Kaisan003 <khai030720038c@gmail.com>
2023-08-16 20:58:37 +0700

    push fail

--- AppData/Models/Cart.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppData.Models
-{
-    public class Cart
-    {
-        public Guid UserID { get; set; }
-        public virtual IEnumerable<CartDetail> cartdetail { get; set; }
-        public virtual User User { get; set; }
-    }
-}
